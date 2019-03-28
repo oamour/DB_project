aff90cf7d37d8fc1fad28246ec3e74e59742aca3
theorangesauce <nickbishop97@gmail.com>
2019-03-27 23:43:40 -0400

    Remove deprecated functions
    Removed deprecated notification functions in dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -200,120 +200,6 @@ function mentor_confirmation( $myconnection, $row){
 	}
 	
 }
-###
-# NOTIFICATIONS
-###
-function get_mentor_notifications($myconnection, $row) {
-	$userid = $row['userID'];
-	$query = "SELECT * FROM sessions WHERE sectionID IN (SELECT sectionID FROM mentorFor WHERE mentorID = $userid) AND sessionID NOT IN (SELECT sessionID FROM participatingIn WHERE userID = $userid)";
-	$result = mysqli_query($myconnection, $query) or die ("Failed to query database");
-	if(mysqli_num_rows($result) > 0) {
-		echo "<h1>Mentor Notifications for Next Week</h1>";
-		echo "<table border='1'>";
-		echo "<tr>";
-		echo "<td>Role</td><td>Course Name</td><td>Section Number</td>";
-		echo "<td>Session Number</td><td>Session Date</td>";
-		echo "<td>Participating Mentee Count</td><td>Participate/Cancel</td>";
-		echo "</tr>";
-		
-		while(($session_info = $result->fetch_array()) != NULL) {
-			# Get section info
-			$query = "SELECT * FROM sections WHERE sectionID = " . $session_info['sectionID'];
-			$section_result = mysqli_query($myconnection, $query) or die ("Failed to query database");
-			$section_info = $section_result->fetch_array();
-			
-			# Get course info
-			$query = "SELECT * FROM courses WHERE courseID = " . $section_info['courseID'];
-			$course_result = mysqli_query($myconnection, $query) or die ("Failed to query database");
-			$course_info = $course_result->fetch_array();
-			
-			echo "<tr>";
-			echo "<td>Mentor</td>";
-			echo "<td>" . $course_info['title'] . "</td>";
-			echo "<td>" . $section_info['sectionID'] . "</td>";
-			echo "<td>" . $session_info['sessionID'] . "</td>";
-			echo "<td>" . $session_info['sessionDate'] . "</td>";
-			
-			#Get participating mentees
-			$query = "SELECT count(userID) FROM participatingIn WHERE sessionID = " . $session_info['sessionID'] . " AND userID IN (SELECT userID FROM menteeFor WHERE sectionID = ". $section_info['sectionID'] . ") GROUP BY sessionID";
-			$mentee_count = mysqli_query($myconnection, $query) or die ("Failed to query database");
-			if(mysqli_num_rows($mentee_count) == 0) {
-				$mentee_count = "N/A";
-			} else {
-				$mentee_count = $mentee_count->fetch_row()[0];
-			}
-			echo "<td>$mentee_count</td>";
-			
-			# Form
-			echo "<td><form method='post' action='dashboard.php'>";
-			echo "<input type='hidden' name='sessionID' value='" . $session_info['sessionID'] . "'>";
-			echo "<input type='hidden' name='sectionID' value='" . $section_info['sectionID'] . "'>";
-			echo "<select name='participating' id='participating'>";
-			echo "<option value='yes'>Participate</option>";
-			echo "<option value='no'>Decline</option>";
-			echo "</select>";
-			echo "<input type='submit' name='submit' id='submit' value='Submit'>";
-			echo "</form></td></tr>";
-		}
-	}
-	
-}
-
-function get_mentee_notifications($myconnection, $row) {
-	$userid = $row['userID'];
-	$query = "SELECT * FROM sessions WHERE sectionID IN (SELECT sectionID FROM menteeFor WHERE menteeID = $userid) AND sessionID NOT IN (SELECT sessionID FROM participatingIn WHERE userID = $userid)";
-	$result = mysqli_query($myconnection, $query);
-	if(mysqli_num_rows($result) > 0) {
-		echo "<h1>Mentee Notifications for Next Week</h1>";
-		echo "<table border='1'>";
-		echo "<tr>";
-		echo "<td>Role</td><td>Course Name</td><td>Section Number</td>";
-		echo "<td>Session Number</td><td>Session Date</td>";
-		echo "<td>Participating Mentee Count</td><td>Participate/Cancel</td>";
-		echo "</tr>";
-		
-		while(($session_info = $result->fetch_array()) != NULL) {
-			# Get section info
-			$query = "SELECT * FROM sections WHERE sectionID = " . $session_info['sectionID'];
-			$section_result = mysqli_query($myconnection, $query) or die ("Failed to query database");
-			$section_info = $section_result->fetch_array();
-			
-			# Get course info
-			$query = "SELECT * FROM courses WHERE courseID = " . $section_info['courseID'];
-			$course_result = mysqli_query($myconnection, $query) or die ("Failed to query database");
-			$course_info = $course_result->fetch_array();
-			
-			echo "<tr>";
-			echo "<td>Mentor</td>";
-			echo "<td>" . $course_info['title'] . "</td>";
-			echo "<td>" . $section_info['sectionID'] . "</td>";
-			echo "<td>" . $session_info['sessionID'] . "</td>";
-			echo "<td>" . $session_info['sessionDate'] . "</td>";
-			
-			#Get participating mentees
-			$query = "SELECT count(userID) FROM participatingIn WHERE sessionID = " . $session_info['sessionID'] . " AND userID IN (SELECT menteeID FROM menteeFor WHERE sectionID = ". $section_info['sectionID'] . ") GROUP BY sessionID";
-			$mentee_count = mysqli_query($myconnection, $query) or die ("Failed to query database");
-			if(mysqli_num_rows($mentee_count) == 0) {
-				$mentee_count = "N/A";
-			} else {
-				$mentee_count = $mentee_count->fetch_row()[0];
-			}
-			echo "<td>$mentee_count</td>";
-			
-			# Form
-			echo "<td><form method='post' action='dashboard.php'>";
-			echo "<input type='hidden' name='sessionID' value='" . $session_info['sessionID'] . "'>";
-			echo "<input type='hidden' name='sectionID' value='" . $section_info['sectionID'] . "'>";
-			echo "<select name='participating' id='participating'>";
-			echo "<option value='yes'>Participate</option>";
-			echo "<option value='no'>Decline</option>";
-			echo "</select>";
-			echo "<input type='submit' name='submit' id='submit' value='Submit'>";
-			echo "</form></td></tr>";
-		}
-	}
-	
-}
 	
 function create_student_dashboard($myconnection, $row) {
 	echo "<a href='dashboard.php'>Student Dashboard</a><br />";
@@ -353,8 +239,6 @@ function create_student_dashboard($myconnection, $row) {
 			</tr>");
 	mentee_confirmation($myconnection, $row);
 	mentor_confirmation($myconnection, $row);
-	get_mentor_notifications($myconnection, $row);
-	get_mentee_notifications($myconnection, $row);
 }
 
 function create_parent_dashboard($myconnection, $row) {
