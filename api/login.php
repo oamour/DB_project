4573f6b4d038ce0c303d77cbebe3a03586f34b9c
theorangesauce <nickbishop97@gmail.com>
2019-04-18 00:22:20 -0400

    Modify api\login.php to support JSON input
    api\login.php takes an email and password as a JSON object, and returns a JSON object containing a key marking success or failure, the email, full name, and user ID of the requested user.

--- api/login.php
+++ api/login.php
@@ -12,27 +12,48 @@ function check_credentials ($username, $pass) {
 	if (mysqli_num_rows($result) > 0) {
 		$row = mysqli_fetch_array($result);
 		if ($username != $row["username"] or !password_verify($pass, $row["hash"])) {
-			return 0;
+			return null;
 		}
-		return $row["userID"];
+		
+		//success, get user info
+		$userid = $row["userID"];
+		$query = "SELECT userID, name, email FROM users WHERE userID = " . $userid;
+		$result = mysqli_query($myconnection, $query) or die("Failed to retrieve user info for " . $username);
+		
+		if(mysqli_num_rows($result) > 0) {
+			$row = mysqli_fetch_array($result);
+			$row["success"] = 1;
+			return json_encode($row);
+		}
+		
 	} else {
-		return 0;
+		return null;
 	}
 }
 
 $session_key = md5("database");
 
-if (isset($_POST)) {
-	$userid = check_credentials($_POST['username'], $_POST['password']);
-	if ($userid != 0) {
-		echo "Success! Logged in as " . $_POST['username'] . "<br />";
-		session_start();
-		$_SESSION[$session_key] = $userid;
-		echo "Redirecting to Dashboard...";
-		header("Location: dashboard.php");
+$value = json_decode(file_get_contents('php://input'));
+
+//DEBUG
+if ($value == null) {
+	$value = (object)[];
+	$value->email = "nickbishop97@gmail.com";
+	$value->pass = "testtest";
+}
+
+if ($value != null) {
+	$email = $value->email;
+	$pass = $value->pass;
+	
+	$user_info = check_credentials($email, $pass);
+	
+	if ($user_info != null) {
+		return $user_info;
 	} else {
-		echo "Failure! Failed to log in as " . $_POST['username'];
-		echo "Please <a href='index.php'>CLICK HERE</a> to return to main page.";
+		$user_info = (object)[];
+		$user_info["success"] = 0;
+		return json_encode($user_info);
 	}
 }
 ?>
