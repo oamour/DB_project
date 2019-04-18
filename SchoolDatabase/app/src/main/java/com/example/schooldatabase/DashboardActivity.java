63f9a40bf832c51ba7200832527720044faa58f7
theorangesauce <nickbishop97@gmail.com>
2019-04-18 15:08:02 -0400

    Prevent users from accessing dashboard without logging in
    Adds code to show a toast to the user and return to MainActivity if the user is not logged in.
    
    Also modified divider in layout

--- SchoolDatabase/app/src/main/java/com/example/schooldatabase/DashboardActivity.java
+++ SchoolDatabase/app/src/main/java/com/example/schooldatabase/DashboardActivity.java
@@ -1,13 +1,34 @@
 package com.example.schooldatabase;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
+    private SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        session = new SessionManager(getApplicationContext());
+
+        if(!session.isLoggedIn()) {
+            Context context = getApplicationContext();
+            CharSequence message = "You do not have permission to access this page";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, message, duration);
+            toast.show();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        //get user info
+        //get whether user is parent, moderator, mentor, mentee
+        //show buttons based on that
     }
 }
