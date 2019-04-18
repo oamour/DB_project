79a1bd942535f1b87e43492c31665d581dc91b6f
theorangesauce <nickbishop97@gmail.com>
2019-04-18 00:12:09 -0400

    Add stub gotoDashboard() function
    Add stub gotoDashboard function to loginActivity, along with code to call the function if the user is already logged in.

--- SchoolDatabase/app/src/main/java/com/example/schooldatabase/LoginActivity.java
+++ SchoolDatabase/app/src/main/java/com/example/schooldatabase/LoginActivity.java
@@ -1,19 +1,41 @@
 package com.example.schooldatabase;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+    private SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        session = new SessionManager(getApplicationContext());
+
+        // if logged in, go straight to dashboard
+        if (session.isLoggedIn()) {
+            gotoDashboard();
+        }
 
         //get intent, extract string
         Intent intent = getIntent();
@@ -27,6 +49,10 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             // leave as default (parent items)
         }
+    }
+    public void gotoDashboard() {
+        Intent intent = getIntent();
+        // TODO: write dashboard activity
     }
 
     public void gotoRegisterPage(View view) {
