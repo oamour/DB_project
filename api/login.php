830d23d6a99abeccb4f092a1e39fd1648d7f1bcd
theorangesauce <nickbishop97@gmail.com>
2019-04-18 00:35:36 -0400

    Make check_credentials() always return JSON object
    Modified check_credentials() function to always return a JSON object containing only necessary info (success, [email, name, userID])

--- api/login.php
+++ api/login.php
@@ -9,51 +9,45 @@ function check_credentials ($username, $pass) {
 	$query = "SELECT * FROM accounts WHERE username = '" . $username . "'";
 	$result = mysqli_query($myconnection, $query) or die("Failed to retrieve account info for " . $username);
 	
+	$user_info = (object)[];
+	$user_info->success = 0;
+	
 	if (mysqli_num_rows($result) > 0) {
 		$row = mysqli_fetch_array($result);
-		if ($username != $row["username"] or !password_verify($pass, $row["hash"])) {
-			return null;
+		if ($username == $row["username"] and password_verify($pass, $row["hash"])) {
+		
+			//account exists, get user info
+			$userid = $row["userID"];
+			$query = "SELECT userID, name, email FROM users WHERE userID = " . $userid;
+			$result = mysqli_query($myconnection, $query) or die("Failed to retrieve user info for " . $username);
+			
+			if(mysqli_num_rows($result) > 0) {
+				$row = mysqli_fetch_array($result);
+				$user_info->name = $row["name"];
+				$user_info->userID = $row["userID"];
+				$user_info->email = $row["email"];
+				$user_info->success = 1;
+			}
 		}
-		
-		//success, get user info
-		$userid = $row["userID"];
-		$query = "SELECT userID, name, email FROM users WHERE userID = " . $userid;
-		$result = mysqli_query($myconnection, $query) or die("Failed to retrieve user info for " . $username);
-		
-		if(mysqli_num_rows($result) > 0) {
-			$row = mysqli_fetch_array($result);
-			$row["success"] = 1;
-			return json_encode($row);
-		}
-		
-	} else {
-		return null;
 	}
+	
+	return json_encode($user_info);
 }
-
-$session_key = md5("database");
 
 $value = json_decode(file_get_contents('php://input'));
 
 //DEBUG
-if ($value == null) {
+/*if ($value == null) {
 	$value = (object)[];
 	$value->email = "nickbishop97@gmail.com";
 	$value->pass = "testtest";
-}
+}*/
 
 if ($value != null) {
 	$email = $value->email;
 	$pass = $value->pass;
 	
 	$user_info = check_credentials($email, $pass);
-	
-	if ($user_info != null) {
-		return $user_info;
-	} else {
-		$user_info = (object)[];
-		$user_info["success"] = 0;
-		return json_encode($user_info);
-	}
+	echo $user_info;
 }
 ?>
