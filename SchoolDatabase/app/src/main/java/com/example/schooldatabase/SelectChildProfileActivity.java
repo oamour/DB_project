31858657941e23ffade9b952a08eb11eb038e60c
theorangesauce <nickbishop97@gmail.com>
2019-04-21 17:45:42 -0400

    Fully implement getChildrenInfo()
    getChildrenInfo() now receives the JSON response and either generates TextAreas and Buttons corresponding to the children in the response, or reveals an element telling the user no children were found.

--- SchoolDatabase/app/src/main/java/com/example/schooldatabase/SelectChildProfileActivity.java
+++ SchoolDatabase/app/src/main/java/com/example/schooldatabase/SelectChildProfileActivity.java
@@ -2,10 +2,13 @@ package com.example.schooldatabase;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,10 +24,12 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class SelectChildProfileActivity extends AppCompatActivity {
     private SessionManager session;
     private RequestQueue queue;
+    private JSONArray children;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +53,10 @@ public class SelectChildProfileActivity extends AppCompatActivity {
             finish();
         }
 
-        //generate TextAreas, buttons corresponding to each child
-
+        getChildrenInfo();
     }
 
-    public void getChildrenInfo(View view) {
+    public void getChildrenInfo() {
         User user = session.getUserDetails();
         String url = MainActivity.HREF + "/code/project/api/children_parent.php";
         JSONArray requestContent = new JSONArray();
@@ -71,12 +75,64 @@ public class SelectChildProfileActivity extends AppCompatActivity {
                     public void onResponse(JSONArray response) {
                         // response
                         Log.d("Response", response.toString());
+                        children = response;
 
                         try {
                             int i = 0;
+                            ConstraintLayout layout = findViewById(R.id.selectChildProfile_layout);
+                            ConstraintSet set = new ConstraintSet();
+
+                            if(response.isNull(0)) {
+                                //no children
+                                TextView textView = (TextView) findViewById(R.id.no_children_found);
+                                textView.setVisibility(View.VISIBLE);
+                            }
                             while (!response.isNull(i)) {
                                 JSONObject child = response.getJSONObject(i);
                                 Log.d("SelectChildProfile", "Child " + i + " is " + child.toString());
+                                //generate textArea, button per child
+                                TextView child_view = new TextView(getApplicationContext());
+                                child_view.setText(child.getString("name"));
+                                child_view.setId(100 + i);
+
+                                //generate button
+                                Button child_button = new Button(getApplicationContext());
+                                child_button.setText(getResources().getString(R.string.change_profile));
+                                child_button.setId(-100-i);
+                                child_button.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        gotoChangeChildProfile(v);
+                                    }
+                                });
+
+                                layout.addView(child_button, 0);
+                                layout.addView(child_view, 0);
+                                set.clone(layout);
+
+                                if(i == 0) {
+                                    //first element, constrain to top
+                                    set.connect(child_view.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP, 60);
+                                    set.connect(child_view.getId(), ConstraintSet.LEFT, layout.getId(), ConstraintSet.LEFT, 60);
+                                } else {
+                                    //not first element, constrain to previous
+                                    TextView prev_view = findViewById(100 + i - 1);
+                                    set.connect(child_view.getId(), ConstraintSet.TOP, prev_view.getId(), ConstraintSet.BOTTOM, 60);
+                                    set.connect(child_view.getId(), ConstraintSet.LEFT, layout.getId(), ConstraintSet.LEFT, 60);
+                                }
+
+
+                                set.connect(child_button.getId(), ConstraintSet.LEFT, R.id.selectChildProfile_guideline, ConstraintSet.RIGHT);
+                                set.connect(child_view.getId(), ConstraintSet.BASELINE, child_button.getId(), ConstraintSet.BASELINE);
+
+                                if(i == 0) {
+                                    set.connect(child_button.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP, 60);
+                                } else {
+                                    Button prev_button = findViewById(-100 - i + 1);
+                                    set.connect(child_button.getId(), ConstraintSet.TOP, prev_button.getId(), ConstraintSet.BOTTOM, 60);
+                                }
+
+                                set.applyTo(layout);
                                 i++;
                             }
                         } catch (JSONException e) {
@@ -100,6 +156,11 @@ public class SelectChildProfileActivity extends AppCompatActivity {
                 }
         );
         queue.add(getRequest);
+    }
+
+    public void gotoChangeChildProfile(View view) {
+        //TODO: Implement changeChildProfile functionality
+        Log.d("SelectChildProfile", "Going to ChangeProfile " + view.getId());
     }
 
 }
