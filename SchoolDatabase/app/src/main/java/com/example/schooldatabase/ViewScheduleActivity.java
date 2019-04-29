d9bfc53347b27bec7d36e813d97f5317af37ab26
theorangesauce <nickbishop97@gmail.com>
2019-04-28 22:50:37 -0400

    Add function getClassInfo()
    getClassInfo() will send the current user's ID to the server, and will receive an array containing all the classes in which the user is enrolled.

--- SchoolDatabase/app/src/main/java/com/example/schooldatabase/ViewScheduleActivity.java
+++ SchoolDatabase/app/src/main/java/com/example/schooldatabase/ViewScheduleActivity.java
@@ -43,5 +43,50 @@ public class ViewScheduleActivity extends AppCompatActivity {
             finish();
         }
 
+        getClassInfo();
     }
+
+    public void getClassInfo() {
+        User user = session.getUserDetails();
+        String url = MainActivity.HREF + "/code/project/api/schedule.php";
+        JSONArray requestContent = new JSONArray();
+        try {
+            requestContent.put(0,  Integer.parseInt(user.getID()));
+        } catch (JSONException e) {
+            Log.d("JsonException", e.toString());
+        }
+
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.POST, url, requestContent,
+                new Response.Listener<JSONArray>()
+                {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // response
+                        Log.d("Response", response.toString());
+
+                        try {
+
+                        } catch (Exception e) {
+                            Log.d("JsonException", e.toString());
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                        Context context = getApplicationContext();
+                        CharSequence text = "Error response!" + error.toString();
+                        int duration = Toast.LENGTH_LONG;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+        );
+        queue.add(getRequest);
+    }
+
 }
