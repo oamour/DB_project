324cb2ffcf01b8aaf94e72be1ae9bf0666d9ca86
theorangesauce <nickbishop97@gmail.com>
2019-04-28 22:49:32 -0400

    Check if user is logged in before using view

--- SchoolDatabase/app/src/main/java/com/example/schooldatabase/ViewScheduleActivity.java
+++ SchoolDatabase/app/src/main/java/com/example/schooldatabase/ViewScheduleActivity.java
@@ -1,13 +1,47 @@
 package com.example.schooldatabase;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class ViewScheduleActivity extends AppCompatActivity {
+    private SessionManager session;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_schedule);
+
+        session = new SessionManager(getApplicationContext());
+        queue = Volley.newRequestQueue(this);
+
+        User user = session.getUserDetails();
+
+        if(!session.isLoggedIn()) {
+            Context context = getApplicationContext();
+            CharSequence message = "You do not have permission to access this page";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, message, duration);
+            toast.show();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
