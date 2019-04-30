9ed23a68799d06f19b83ec9d5a44f0ef89c8dbee
theorangesauce <nickbishop97@gmail.com>
2019-04-30 13:14:42 -0400

    Remove code relating to DisplayMessageActivity
    DisplayMessageActivity was a demo activity created while learning how to use Android Studio, can safely be removed without affecting rest of project.

--- SchoolDatabase/app/src/main/java/com/example/schooldatabase/MainActivity.java
+++ SchoolDatabase/app/src/main/java/com/example/schooldatabase/MainActivity.java
@@ -4,10 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.schooldatabase.MESSAGE";
     public static final String LOGIN_TYPE = "com.example.schooldatabase.LOGIN_TYPE";
     public static final String HREF = "http://10.253.26.185";
 
@@ -16,14 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /*/** when user presses Send button...
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }*/
+
     public void gotoLoginParent(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra(LOGIN_TYPE, "parent");
